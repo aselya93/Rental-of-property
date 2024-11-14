9946437e75c5432413b8711c4968bb3414144b24
«aselya93» <asel_hbk@mail.ru>
2024-11-14 11:33:24 +0300

    client + server

---
+++ client/src/components/Registration.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
+
+
+import React from 'react';
+
+function Registration(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Registration;
