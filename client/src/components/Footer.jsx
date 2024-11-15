38fa89c2f69957364d1f8e795f3b5178631f1cb5
YuliaToros <yulia.elb@gmail.com>
2024-11-15 16:30:39 +0300

    oh

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
                 <div className="row">
                     <div className="col-md-12">
                         <h5>Контакты</h5>
-                        <p>Адрес: город Джаваскриптбург, ул. Реактов, 123</p>
+                        <p>Адрес: город Джаваскриптбург, ул. Реактовая, 123</p>
                         <p>Email: <a href="mailto:info@example.com" className="text-white">info@example.com</a></p>
 
                         <p> © 2024 Куда я жмав Inc</p>
